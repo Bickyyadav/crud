f3eca68f1b7fa9930156e5762acbb7c3d00a84c2
Bicky yadav <ybicky86@gmail.com>
2025-05-01 01:18:58 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import { prismaClient } from './prismaClient/prismaclient';
+
+const app = express();
+
+app.get('/', () => {
+  console.log('fasdfasd');
+});
+
+app.post('/create-user', async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  if (!email || !password || !firstName || !lastName) {
+    res.status(400).json({
+      success: false,
+      message: 'all fields are required',
+    });
+    return;
+  }
+  const data = {
+    email,
+    password,
+    firstName,
+    lastName,
+  };
+  const user = await prismaClient.user.create({
+    data: data,
+  });
+
+  res.status(200).json({
+    data: user,
+    success: true,
+    message: 'User created successfully',
+  });
+});
+
+app.get('/get-allUser', async (req, res) => {
+  const users = await prismaClient.user.findMany();
+  res.json({
+    message: 'successfully get All user',
+    users,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is running on port number 3000');
+});
