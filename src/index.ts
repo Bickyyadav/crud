90eb08b344f48a9223d2f2c9d1fe8d3243f4765b
Bicky yadav <ybicky86@gmail.com>
2025-05-01 14:49:24 +0530

    update schema completd

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { prismaClient } from './prismaClient/prismaclient';
+import bcrypt from 'bcryptjs';
 
 import dotenv from 'dotenv';
 dotenv.config({});
@@ -20,9 +21,10 @@ app.post('/create-user', async (req, res) => {
     });
     return;
   }
+  const hashPassword = await bcrypt.hash(password, 10);
   const data = {
     email,
-    password,
+    password: hashPassword,
     firstName,
     lastName,
   };
@@ -43,6 +45,86 @@ app.get('/get-allUser', async (req, res) => {
     message: 'successfully get All user',
     users,
   });
+});
+
+app.post('/delete-user', async (req, res) => {
+  try {
+    const { email } = req.body;
+    console.log('🚀 ~ app.post ~ email:', email);
+    if (!email) {
+      res.json({
+        success: false,
+        message: 'All fields are required',
+      });
+      return;
+    }
+    const user = await prismaClient.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      res.json({
+        success: false,
+        message: 'user does not exist',
+      });
+      return;
+    }
+    await prismaClient.user.delete({
+      where: {
+        email,
+      },
+    });
+    res.json({
+      message: 'user deleted successfully',
+    });
+    return;
+  } catch (error) {
+    console.log('🚀 ~ app.post ~ error:', error);
+  }
+});
+
+app.post('/update-user', async (req, res) => {
+  try {
+    const { email, password, firstName, lastName } = req.body;
+    if (!email) {
+      res.json({
+        success: false,
+        message: 'All fields are required',
+      });
+      return;
+    }
+    const user = await prismaClient.user.findUnique({ where: { email } });
+    if (!user) {
+      res.json({
+        success: false,
+        message: 'user does not exist',
+      });
+      return;
+    }
+    //updating the field
+    type update = {
+      password?: string;
+      firstName?: string;
+      lastName?: string;
+    };
+
+    const updateData: update = {};
+    if (password) updateData.password = password;
+    if (firstName) updateData.firstName = firstName;
+    if (lastName) updateData.lastName = lastName;
+    const updateUser = await prismaClient.user.update({
+      where: {
+        email,
+      },
+      data: updateData,
+    });
+    res.json({
+      success: true,
+      message: 'User updated successfully',
+      data: updateUser,
+    });
+  } catch (error) {
+    console.log('🚀 ~ app.get ~ error:', error);
+  }
 });
 
 app.listen(3000, () => {
